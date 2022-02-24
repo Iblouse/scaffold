a170216cdf420ab8d4074ece7c0ef132d015ef9c
Iblouse <ibrahibar@aol.com>
2022-02-24 16:59:55 +0000

    adding azure version

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-# add: docstring 
-def add():
-    print('Hello')
+"""This is the hello program"""
+
+
+def add(num1, num2):
+    """This is the add function"""
+    return num1 + num2
+
+RES = add(1, 2)
+print(f'This the sum of 1 and 2 is {RES}')
--- test_hello.py
+++ test_hello.py
@@ -1,3 +1,7 @@
-from hello import add
-def test_hello():
+from hello import  add 
+
+### Run to see failed test
+def test_add():
     assert add(1, 2) == 3
+
+
